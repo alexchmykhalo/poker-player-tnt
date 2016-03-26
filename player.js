3956c3a18b80c666061d7437f76b04359ca60ffe
mpylypovych <pylypovych@gmail.com>
2016-03-26 12:52:40 +0200

    1

--- player.js
+++ player.js
@@ -4,7 +4,8 @@ module.exports = {
   VERSION: "Default JavaScript folding player",
 
   bet_request: function(game_state, bet) {
-    bet(350);
+
+    bet(Math.max(game_state.current_buy_in * 2, 350);
     console.log(game_state);
   },
 
