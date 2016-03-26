4c456b875d3cbc7d3f88a253fb9c51d98c290a1d
taras.teslyuk <taras.teslyuk@gmail.com>
2016-03-26 14:36:36 +0200

    revert;

--- player.js
+++ player.js
@@ -1,5 +1,4 @@
 
-var RM = require('./rainman')
 
 module.exports = {
 
@@ -9,86 +8,28 @@ module.exports = {
 
   bet_request: function(game_state, bet) {
 
-    var rainman = require('./rainman')(game_state);
+  try {
+    //console.log(rankHand(game_state));
+      var rainman = require('./rainman');
 
-/*
-var start = new Date(); console.log(start);
-    for(var i = 0 ; i< 47;++i){
-
-    var rainman1 = RM(game_state);
-  }
-  var end = new Date(); console.log(end);
-  console.log(end-start);
-*/
-/*
-var currentRank = RM(game_state);
-
-var suits = ["diamonds", "spades", "clubs", "hearts"];
-var ranks = ["2","3","4","5","6","7","8","9", "T", "J", "Q", "K", "A"];
-
-suits.forEach(function(suit) {
-  ranks.forEach(function(rank)
-  {
-    console.log(suit+' '+rank);
-  });
-});*/
-
-    /*
-  	var pairCount = this.pairCount(game_state);
-  	var card3Count = this.has3Count(game_state);
-  	var card4Count = this.has4Count(game_state);
-  	var cardNumber = this.getAllCards(game_state).length;
-  	var player = this.getPlayer(game_state);
-  	if(cardNumber == 2 && pairCount == 1){
-  		//all in
-  		bet(player.stack);
-  	}else if(cardNumber >= 5 && pairCount == 2){
-  		//all in
-  		bet(player.stack);
-  	}else if(card3Count >= 1){
-  		//all in
-  		bet(player.stack);
-  	}else if(card4Count >= 1){
-  		//all in
-  		bet(player.stack);
-  	}else{
-  		var currentBet = Math.max(game_state.minimum_raise, 350);
-  		if(currentBet > player.stack) currentBet = player.stack;
-	    bet(currentBet);
-	    // console.log(game_state);
-    }
-*/
-
-
-try {
-	//console.log(rankHand(game_state));
-
-
-    if (rankHand(game_state) == 0)
-    {
-      bet(Math.min(Math.max(game_state.minimum_raise * 2, 350),game_state.players[game_state.in_action].stack))
-    } else {
-
-      if (rainman.rank > 0 )
+      if (rankHand(game_state) == 0)
       {
-        //if (rainman.rank == 1 && rainman.value > 7)
-          bet(Math.min(Math.max(game_state.minimum_raise * 2, 350),game_state.players[game_state.in_action].stack))
+        bet(Math.min(Math.max(game_state.minimum_raise * 2, 350),game_state.players[game_state.in_action].stack));
       } else {
-          bet(0);
+
+        if (rainman.rank > 0 )
+        {
+          //if (rainman.rank == 1 && rainman.value > 7)
+            bet(Math.min(Math.max(game_state.minimum_raise * 2, 350),game_state.players[game_state.in_action].stack));
+        } else {
+            bet(0);
+        }
       }
-    }
-}
-catch(err) {
-	bet(0);
-}
-
-    
-    //console.log(game_state);
-
-
-
-
-
+  }
+  catch(err) {
+    bet(0);
+  }
+      //console.log(game_state);
   },
 
   showdown: function(game_state) {
@@ -96,94 +37,22 @@ catch(err) {
   },
 
   getPlayer: function(game_state){
-  	return game_state.players[game_state.in_action];
+    return game_state.players[game_state.in_action];
   },
 
   getMyCards: function(game_state){
-  	return this.getPlayer.hole_cards;
+    return this.getPlayer.hole_cards;
   },
 
   getCommunityCards: function(game_state){
-  	return game_state.community_cards;
+    return game_state.community_cards;
   },
 
   getAllCards : function(game_state){
-		var myCards = this.getMyCards(game_state);
-		var otherCards = this.getCommunityCards(game_state);
-		return myCards.concat(otherCards);
-	},
-
-	pairCount : function(game_state){
-		var allCards = this.getAllCards(game_state);
-
-		var ranks = [];
-		allCards.forEach(function (e) {
-			ranks.push(e.rank);	
-		});
-
-		ranks = ranks.sort();
-		//console.log(ranks);
-		var arrayLength = ranks.length;
-		var pairCount = 0;
-		for (var i = 0; i < arrayLength; i++) {
-			if(i < arrayLength - 1){
-				if(ranks[i]==ranks[i+1]){
-					pairCount++;
-					i++;
-				}
-			}
-		}
-
-		return pairCount;
-	},
-
-	has3Cards : function(game_state){
-		var allCards = this.getAllCards(game_state);
-
-		var ranks = [];
-		allCards.forEach(function (e) {
-			ranks.push(e.rank);	
-		});
-
-		ranks = ranks.sort();
-		//console.log(ranks);
-		var arrayLength = ranks.length;
-		var card3Count = 0;
-		for (var i = 0; i < arrayLength; i++) {
-			if(i < arrayLength - 1){
-				if(ranks[i]==ranks[i+1] && ranks[i]==ranks[i+2]){
-					card3Count++;
-					i += 2;
-				}
-			}
-		}
-
-		return card3Count;
-	},
-
-	has4Cards : function(game_state){
-		var allCards = this.getAllCards(game_state);
-
-		var ranks = [];
-		allCards.forEach(function (e) {
-			ranks.push(e.rank);	
-		});
-
-		ranks = ranks.sort();
-		//console.log(ranks);
-		var arrayLength = ranks.length;
-		var card4Count = 0;
-		for (var i = 0; i < arrayLength; i++) {
-			if(i < arrayLength - 1){
-				if(ranks[i]==ranks[i+1] && ranks[i]==ranks[i+2] && ranks[i]==ranks[i+3]){
-					card4Count++;
-					i += 3;
-				}
-			}
-		}
-
-		return card4Count;
-	}
+    var myCards = this.getMyCards(game_state);
+    var otherCards = this.getCommunityCards(game_state);
+    return myCards.concat(otherCards);
+  },
 
 };
 
